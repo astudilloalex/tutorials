fc2d22fe9c24994dbac022cced6d74a927d7644e
astudilloalex <astudilloalex1@gmail.com>
2021-05-09 17:52:12 -0500

    Add constants dart file with static members

---
+++ lib/common/constants.dart
@@ -0,0 +1,11 @@
+class Routes {
+  static const String home = '/';
+  static const String notesHome = '/notes';
+  static const String editNote = '/edit-note';
+  static const String searchNote = '/search-note';
+}
+
+enum EditMode {
+  ADD,
+  UPDATE,
+}
